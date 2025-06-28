9118456cfbb554dc5feeefc16edbf8f736273c14
Sreekanth <spayyavula@gmail.com>
2025-06-27 22:48:21 -0700

    Fix configuration file errors

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['react', 'react-dom']
+    include: ['react', 'react-dom'],
+    exclude: ['lucide-react'],
   },
   build: {
     target: 'esnext'
   }
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
