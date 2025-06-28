056018976c4f18ce9caa80aeef0618fea0280a1a
Sreekanth <spayyavula@gmail.com>
2025-06-28 05:26:36 -0700

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import AndroidBuildLogs from './components/AndroidBuildLogs.tsx';
 import { StripeProvider } from './components/StripeProvider.tsx';
 import './index.css';
 
